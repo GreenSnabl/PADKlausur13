07e8d4ce91cbd1934abeecb9d2db92cc0a0f8640
Dennis <dennis.imhof@googlemail.com>
2017-10-20 22:03:07 +0200

    header only

---
+++ Theatre.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Theatre.h
+ * Author: dennis
+ *
+ * Created on October 20, 2017, 9:53 PM
+ */
+
+#ifndef THEATRE_H
+#define THEATRE_H
+
+
+
+#endif /* THEATRE_H */
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: dennis
+ *
+ * Created on October 20, 2017, 9:48 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
